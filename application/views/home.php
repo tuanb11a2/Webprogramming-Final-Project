f0d9a267a0c83066f150445ae1bd78a5ee3ea27f
tuancon <tuanb11a2@gmail.com>
2021-05-22 13:36:42 +0700

    Done Latest Publish Section Homepage

--- application/views/home.php
+++ application/views/home.php
@@ -3,7 +3,7 @@
 	</div>	
 
 	<div class="block-3">
-		<h2>Best Selling Books Ever</h2>
+		<h1>Best Selling Books Ever</h1>
 		<?php include('slider/card_slider_2.html') ?>
 	</div>	
 
@@ -18,6 +18,14 @@
 			style="float: right" 
 			>
 		</div>
+	</div>	
+
+	<div class="block-4">
+		<div class="block-4-h1-div">
+			<div class="block-4-left"><h1>Latest Published items</h1></div>
+			<div class="block-4-right"><?php include('button/latest-publish-button.html') ?></div>
+		</div>
+		<?php include('card/card-div.html') ?>
 	</div>	
 
 	
