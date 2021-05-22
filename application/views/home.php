68c72041f6a4ac1a8026421d0659d8b31ec33a02
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-05-22 19:57:01 +0700

    FIX bug Fp-16

--- application/views/home.php
+++ application/views/home.php
@@ -1,25 +1,20 @@
-	<div class="block-1">	
+	<div class="block-1">
 		<?php include('slider/slider.html') ?>
-	</div>	
+	</div>
 
 	<div class="block-3">
 		<h2>Best Selling Books Ever</h2>
 		<?php include('slider/card_slider_2.html') ?>
-	</div>	
+	</div>
 
-	<div class="block-2">	
-		<div class="block-2-left">
-			<h1 style="font-weight: bold; margin-top: -10px; padding-bottom: 10px;">Feature this week</h1>
-			<?php include('slider/slider1.html') ?>	
+	<div class="block-2">
+		<h1 style="font-weight: bold; margin-top: -10px; padding-bottom: 10px;">Feature this week</h1>
+		<div class="block-2-content" style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
+			<div class="block-2-left" style="flex-basis: 70%;">
+				<?php include('slider/slider1.html') ?>
+			</div>
+			<div class="block-2-right" style="flex-basis: 30%;">
+				<img src="https://preview.colorlib.com/theme/abcbook/assets/img/gallery/xad.jpg.pagespeed.ic.WvnALPgj6F.webp" width=80% style="float: right">
+			</div>
 		</div>
-		<div class="block-2-right" >
-			<img src="https://preview.colorlib.com/theme/abcbook/assets/img/gallery/xad.jpg.pagespeed.ic.WvnALPgj6F.webp"
-			width =80% 
-			style="float: right" 
-			>
-		</div>
-	</div>	
-
-	
-
-
+	</div>
