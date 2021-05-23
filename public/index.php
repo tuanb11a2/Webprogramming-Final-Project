2eb83b40a346d1180bd9fb5f123befb1b8dd250f
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-05-23 10:51:56 +0700

    beautify index.html

--- public/index.php
+++ public/index.php
@@ -1,29 +1,30 @@
-<?php	
+<?php
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
-if(isset($_GET['url'])){
-    $url = $_GET['url'];
+if (isset($_GET['url'])) {
+	$url = $_GET['url'];
 }
 
 
 //require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
-require_once (ROOT . DS . 'library' . DS . 'Route.php');
-require_once (ROOT . DS . 'library' . DS . 'Controller.php');
+require_once(ROOT . DS . 'library' . DS . 'Route.php');
+require_once(ROOT . DS . 'library' . DS . 'Controller.php');
 
 //$init = new Route();
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
 
 	<title>Home page</title>
 	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="css/slider.css" type="text/css">
 	<link rel="stylesheet" href="css/slider1.css" type="text/css">
 	<link rel="stylesheet" href="css/home.css" type="text/css">
@@ -34,19 +35,20 @@ require_once (ROOT . DS . 'library' . DS . 'Controller.php');
 	<link rel="stylesheet" href="css/login.css" type="text/css">
 	<link rel="stylesheet" href="css/card-slider-2.css" type="text/css">
 	<link rel="stylesheet" href="css/latest-publish-button.css" type="text/css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 </head>
+
 <body>
 	<?php
-  require_once '../application/views/header/header.html';
-  $init = new Route();
-  require_once '../application/views/footer/footer.html';
-  ?>
+	require_once '../application/views/header/header.html';
+	$init = new Route();
+	require_once '../application/views/footer/footer.html';
+	?>
 	<script type="text/javascript" src="js/slider.js"></script>
 	<script type="text/javascript" src="js/slider1.js"></script>
 	<script type="text/javascript" src="js/header.js"></script>
 	<script src="js/login.js"></script>
 	<script type="text/javascript" src="js/card-slider.js"></script>
 </body>
-</html>
 
+</html>
