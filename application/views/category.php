8bd27d57c6f87734c961bff9c5c8d91c8d045089
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-05-23 09:59:57 +0700

    Update Category

--- application/views/category.php
+++ application/views/category.php
@@ -5,7 +5,19 @@
 <div class="category__product">
 	<div class="category__product__filter">
 		<div class="category__product__filter__by__category">
-			
+			<h3>Filter By Category</h3>
+			<label class="filter__by__category__container">Category 1
+				<input type="checkbox">
+				<span class="filter__by__category__checkmark"></span>
+			</label>
+			<label class="filter__by__category__container">Category 2
+				<input type="checkbox">
+				<span class="filter__by__category__checkmark"></span>
+			</label>
+			<label class="filter__by__category__container">Category 3
+				<input type="checkbox">
+				<span class="filter__by__category__checkmark"></span>
+			</label>
 		</div>
 	</div>
 	<div class="category__product__cards">
