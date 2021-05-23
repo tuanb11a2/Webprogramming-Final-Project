954c9cb20d065cf931a67115d923674e3f2e0243
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-05-23 16:18:00 +0700

    Done FP-17,19,20

--- application/views/category.php
+++ application/views/category.php
@@ -6,17 +6,48 @@
 	<div class="category__product__filter">
 		<div class="category__product__filter__by__category">
 			<h3>Filter By Category</h3>
-			<label class="filter__by__category__container">Category 1
+			<label class="filter__by__category__container"><p>Category 1</p>
+				<input type="checkbox" checked="check">
+				<span class="filter__by__category__checkmark"></span>
+			</label>
+			<label class="filter__by__category__container"><p>Category 2</p>
 				<input type="checkbox">
 				<span class="filter__by__category__checkmark"></span>
 			</label>
-			<label class="filter__by__category__container">Category 2
+			<label class="filter__by__category__container"><p>Category 3</p>
 				<input type="checkbox">
 				<span class="filter__by__category__checkmark"></span>
 			</label>
-			<label class="filter__by__category__container">Category 3
-				<input type="checkbox">
-				<span class="filter__by__category__checkmark"></span>
+		</div>
+		<div class="category__product__filter__by__publisher">
+			<h3>Filter By Publisher</h3>
+			<label class="filter__by__publisher__container"><p>NXB Kim Dong</p>
+				<input type="radio" checked="check" name="publisher_checked">
+				<span class="filter__by__publisher__checkmark"></span>
+			</label>
+			<label class="filter__by__publisher__container"><p>NXB Ha Noi</p>
+				<input type="radio" name="publisher_checked">
+				<span class="filter__by__publisher__checkmark"></span>
+			</label>
+			<label class="filter__by__publisher__container"><p>NXB Tuan Le</p>
+				<input type="radio" name="publisher_checked">
+				<span class="filter__by__publisher__checkmark"></span>
+			</label>
+		</div>
+
+		<div class="category__product__filter__by__author">
+			<h3>Filter By Author</h3>
+			<label class="filter__by__author__container"><p>Pham Nhat Linh</p>
+				<input type="radio" checked="check" name="author_checked">
+				<span class="filter__by__author__checkmark"></span>
+			</label>
+			<label class="filter__by__author__container"><p>Le Anh Tuan</p>
+				<input type="radio" name="author_checked">
+				<span class="filter__by__author__checkmark"></span>
+			</label>
+			<label class="filter__by__author__container"><p>Pham Thanh Dat</p>
+				<input type="radio" name="author_checked">
+				<span class="filter__by__author__checkmark"></span>
 			</label>
 		</div>
 	</div>
