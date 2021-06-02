72765d02c0568ec455d346484ccdb2d00103844a
Minh Chau <chau.ntm184238@sis.hust.edu.vn>
2021-06-02 23:57:08 +0700

    Add about page (have not merge to main page)

---
+++ application/views/aboutus/aboutus.php
@@ -0,0 +1,65 @@
+<body>
+  <link rel="stylesheet" href="aboutus.css"> 
+    <header class="masthead">
+        <p class="masthead-intro">Hi! We are</p> 
+        <h1 class="masthead-heading">BlackPink</h1>
+    </header>
+  
+<!-- ***** About me ***** -->
+
+<section class="introduction">
+  <div class="starcontainer">
+    <h2>About us</h2>
+    <i class="fa fa-star fa-2x"></i>
+    <hr class="star"></hr>
+  </div>
+    <p>Here is a paragraph about us</p>
+
+    <p>And this is another paragraph</p>
+</section>
+
+<section class="about">
+    <h1>About this project</h1>
+    <p>This is a kewt project for the final exam</p>
+</section>
+
+<!-- ***** Portfolio section ***** -->
+
+<section id="portfolio">
+  <div class="starcontainerportfolio">
+    <h2>Our team</h2>
+    <i class="fa fa-star fa-2x"></i>
+    <hr class="starportfolio"></hr>
+  </div>
+ <div id="container">
+
+
+    <div class="box">
+        <img src="./datpth.png" class="img-responsive img-thumbnail" alt="Responsive image" />
+             <p align="center">Dat Pham Thanh</p>
+             <p>Team leader. Has the ability to cover both the sky and earth</p>
+             <button class="btn"><a href="#">Visit site</a></button>
+
+    </div>
+    <div class="box">
+        <img src="./tuan.png" class="img-responsive img-thumbnail" alt="Responsive image" />
+            <p align="center" font-size="20">Tuan Le Anh</p>
+             <p>Half-imposter. Can be both a blackhat and a whitehat, just depend on what he wants</p>
+              <button class="btn"><a href="#">Visit site</a></button>
+    </div>
+    <div class="box">
+        <img src="./tuanlinh.png" class="img-responsive img-thumbnail" alt="Responsive image" />
+            <p align="center" font-size="20">Linh Pham Tuan</p>
+             <p>Rainbow God. Love animals. Sometimes does shit, but somehow he can turn it into shits</p>
+              <button class="btn"><a href="#">Visit site</a></button>
+    </div>
+    <div class="box">
+        <img src="./chau.png" class="img-responsive img-thumbnail" alt="Responsive image" />
+            <p align="center" font-size="20">Chau Nguyen Thi Minh</p>
+             <p>Garbage collector. Has nothing to do more than playing around with the lockers</p>
+              <button class="btn"><a href="#">Visit site</a></button>
+    </div>
+</div>
+</section>
+
+</body>
