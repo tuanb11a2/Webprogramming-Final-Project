08677c25c8a69562f655209d84f9fef869d4f657
Minh Chau <chau.ntm184238@sis.hust.edu.vn>
2021-06-04 19:56:29 +0700

    Adjusting about pages

--- application/views/aboutus/aboutus.php
+++ application/views/aboutus/aboutus.php
@@ -1,16 +1,13 @@
 <body>
     <header class="masthead">
-        <p class="masthead-intro">Hi! We are</p> 
-        <h1 class="masthead-heading">BlackPink</h1>
+        <p class="masthead-intro">Hi! We are <h1 class="masthead-heading">BlackPink</h1></p>
     </header>
-  
-<!-- ***** About me ***** -->
 
 <section class="introduction">
   <div class="starcontainer">
     <h2>About us</h2>
     <i class="fa fa-star fa-2x"></i>
-    <hr class="star"></hr>
+    <hr class="starintro"></hr>
   </div>
     <p>Here is a paragraph about us</p>
 
@@ -22,39 +19,37 @@
     <p>This is a kewt project for the final exam</p>
 </section>
 
-<!-- ***** Portfolio section ***** -->
-
 <section id="portfolio">
-  <div class="starcontainerportfolio">
+  <div class="starcontainer">
     <h2>Our team</h2>
     <i class="fa fa-star fa-2x"></i>
-    <hr class="starportfolio"></hr>
+    <hr class="starportfo"></hr>
   </div>
  <div id="container">
 
 
     <div class="box">
         <img src="<?php echo LINK; ?>/image/datpth.png" class="img-responsive img-thumbnail" alt="Responsive image" />
-             <p align="center">Dat Pham Thanh</p>
+             <p class="membername">Dat Pham Thanh</p>
              <p>Team leader. Has the ability to cover both the sky and earth</p>
              <button class="btn"><a href="#">Visit site</a></button>
 
     </div>
     <div class="box">
         <img src="<?php echo LINK; ?>/image/tuan.png" class="img-responsive img-thumbnail" alt="Responsive image" />
-            <p align="center" font-size="20">Tuan Le Anh</p>
+            <p class="membername">Tuan Le Anh</p>
              <p>Half-imposter. Can be both a blackhat and a whitehat, just depend on what he wants</p>
               <button class="btn"><a href="#">Visit site</a></button>
     </div>
     <div class="box">
         <img src="<?php echo LINK; ?>/image/tuanlinh.png" class="img-responsive img-thumbnail" alt="Responsive image" />
-            <p align="center" font-size="20">Linh Pham Tuan</p>
+            <p class="membername">Linh Pham Tuan</p>
              <p>Rainbow God. Love animals. Sometimes does shit, but somehow he can turn it into shits</p>
               <button class="btn"><a href="#">Visit site</a></button>
     </div>
     <div class="box">
         <img src="<?php echo LINK; ?>/image/chau.png" class="img-responsive img-thumbnail" alt="Responsive image" />
-            <p align="center" font-size="20">Chau Nguyen Thi Minh</p>
+            <p class="membername">Chau Nguyen Thi Minh</p>
              <p>Garbage collector. Has nothing to do more than playing around with the lockers</p>
               <button class="btn"><a href="#">Visit site</a></button>
     </div>
