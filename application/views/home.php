c4a6301562e0f65acdff298addf4c049e246337f
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-06-08 17:27:38 +0700

    init commit

--- application/views/home.php
+++ application/views/home.php
@@ -1,31 +1,32 @@
-<title>Home page</title>	
+<title>Home Page</title>	
+	<!-- 3 cai dau -->
 	<div class="block-1">
-		<?php include('slider/slider.html') ?>
+		<?php include('slider/slider.php') ?>
 	</div>
-
+	<!-- 6-9 cai cuoi  -->
 	<div class="block-3">
-		<h1>Best Selling Books Ever</h1>
+		<h1>Our Best Books</h1>
 		<?php include('slider/card_slider_2.php') ?>
 	</div>
-
+	<!-- 5 cai dau  -->
 	<div class="block-2">
 		<h1 style="font-weight: bold; margin-top: -10px; padding-bottom: 10px;">Feature this week</h1>
 		<div class="block-2-content" style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
 			<div class="block-2-left" style="flex-basis: 70%;">
-				<?php include('slider/slider1.html') ?>
+				<?php include('slider/slider1.php') ?>
 			</div>
 			<div class="block-2-right" style="flex-basis: 30%;">
 				<img src="https://preview.colorlib.com/theme/abcbook/assets/img/gallery/xad.jpg.pagespeed.ic.WvnALPgj6F.webp" width=80% style="float: right">
 			</div>
 		</div>
 	</div>
-
+	<!-- 6 cai cuoi  -->
 	<div class="block-4">
 		<div class="block-4-h1-div">
 			<div class="block-4-left">
-				<h1>Latest Published items</h1>
+				<h1>Latest Published Books</h1>
 			</div>
-			<div class="block-4-right"><?php include('button/latest-publish-button.html') ?></div>
+			<div class="block-4-right"></div>
 		</div>
 		<?php include('card/card-div.php') ?>
 	</div>
