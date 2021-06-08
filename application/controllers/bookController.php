b7f2d5c5a2ef0a5ac448d915985caeea973ac84a
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-06-08 16:47:40 +0700

    search bar prevent xss attack

--- application/controllers/bookController.php
+++ application/controllers/bookController.php
@@ -31,7 +31,14 @@
         {
 //            var_dump($_POST);
             $bookName = $_POST["search"];
-
+			// only allow char, space, ;, ',' and digit
+			$bookName = preg_replace('/[^A-Za-z0-9:,\s]/', ' ', $bookName);
+			$bookName = trim($bookName);
+			if($bookName==""){
+				header("Location: ".LINK."/book/");
+			}
+			else{
+			$_POST["search"] = $bookName;
             $books = $this->userModel->getBookByName($bookName);
 			$author = $this->userModel->getAllAuthor();
 			$publisher = $this->userModel->getAllPublisher();
@@ -46,5 +53,6 @@
 			$this->view('category4');
 			$this->view('card/category-card-div', $books);
 			$this->view('category5');
+			}
         }
 	}
