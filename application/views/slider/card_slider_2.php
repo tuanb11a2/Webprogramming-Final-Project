89154ae583d9e7b30c0394a6a0940ad97a037d20
nhatlinh4459 <64762945+nhatlinh4459@users.noreply.github.com>
2021-06-08 18:19:26 +0700

    Done dump data to home

--- application/views/slider/card_slider_2.php
+++ application/views/slider/card_slider_2.php
@@ -44,14 +44,13 @@
 <div class="carousel-row">
     <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
         <div class="MultiCarousel-inner">
-
             <?php
                 if(isset($data)){
                     foreach($data as $bookData){
                         foreach($bookData as $value){  ?>
                             <div class="card-item" id="card-item-1">
                                 <div class="pad15">
-                                    <div class="card" onclick='location.href="<?php echo LINK."/detail/".$value["book_id"] ?>";'>
+                                    <div class="card card-div-item" onclick='location.href="<?php echo LINK."/detail/".$value["book_id"] ?>";'>
                                         <div class="card-img">
                                             <a href="<?php echo LINK."/detail/".$value["book_id"] ?>">
                                                 <img src="<?php echo LINK."/".$value["thumbnail_address"]; ?>" alt="Card example">
